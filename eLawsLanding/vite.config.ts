8b7c8764312464983044f879bdedbeded6211ad4
Maksymilian Dylag <maks.dylag@bee2code.com>
2026-01-23 01:12:51 +0100

    fix: error fixes

--- eLawsLanding/vite.config.ts
+++ eLawsLanding/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('@mui') || id.includes('@emotion')) return 'mui';
+                        if (id.includes('firebase')) return 'firebase';
+                        if (id.includes('framer-motion')) return 'motion';
+                        if (id.includes('react-router')) return 'router';
+                        if (id.includes('react')) return 'react';
+                        return 'vendor';
+                    }
+                    return undefined;
+                },
+            },
+        },
+    },
+});
