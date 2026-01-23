950d42972121030faea3bcc38e554aae4909a5db
Maksymilian Dylag <maks.dylag@bee2code.com>
2026-01-23 01:14:58 +0100

    fix: vite changes

--- eLawsLanding/vite.config.ts
+++ eLawsLanding/vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
                         if (id.includes('@mui') || id.includes('@emotion')) return 'mui';
+                        if (id.includes('firebase/app')) return 'firebase-app';
+                        if (id.includes('firebase/auth')) return 'firebase-auth';
+                        if (id.includes('firebase/firestore')) return 'firebase-firestore';
+                        if (id.includes('firebase/storage')) return 'firebase-storage';
+                        if (id.includes('firebase/analytics')) return 'firebase-analytics';
                         if (id.includes('firebase')) return 'firebase';
                         if (id.includes('framer-motion')) return 'motion';
                         if (id.includes('react-router')) return 'router';
